56c0501c0feefa89fe9ce7f398f633ce58837bf8
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-05-25 23:41:23 +0900

    0.0.7 feat: upload requests code

--- common/__init__.py
+++ common/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.6.1'
+__version__ = '0.0.7'
 
 def hello_python():
     print("Hello Python!")
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfmodule',
-    version='0.0.6.1',
+    version='0.0.7',
     description='data management project common features module',
     author='rimmoyee',
     author_email='rimmoyee@gmail.com',
     url='',
-    install_requires=['boto3', 'b64uuid', 'cryptography'],
+    install_requires=['boto3', 'b64uuid', 'cryptography', 'requests'],
     packages=find_packages(exclude=[]),
     keywords=['dfmodule', 'dfmodule777'],
     python_requires='>=3.6',
